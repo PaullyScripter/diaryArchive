236b703c08f9217196ece392d8758afc13dbf072
PaullyScripter <bquoc6834@gmail.com>
2026-06-26 22:21:33 -0700

    fix: bleach CSS sanitizer allows font-family, font-size, style tags

--- backend/app/core/sanitize.py
+++ backend/app/core/sanitize.py
@@ -1,4 +1,5 @@
 import bleach
+from bleach.css_sanitizer import CSSSanitizer
 
 ALLOWED_TAGS = {
     "p", "h1", "h2", "h3", "h4", "h5", "h6",
@@ -9,10 +10,23 @@ ALLOWED_TAGS = {
 }
 
 ALLOWED_ATTRIBUTES = {
-    "*": ["class"],
+    "*": ["class", "style"],
     "a": ["href", "target", "rel"],
     "img": ["src", "alt", "width", "height"],
 }
+
+ALLOWED_CSS_PROPERTIES = frozenset({
+    "font-family", "font-size", "font-weight", "font-style",
+    "color", "background-color", "background",
+    "text-align", "text-decoration", "text-indent",
+    "line-height", "letter-spacing",
+    "margin", "margin-left", "margin-right", "margin-top", "margin-bottom",
+    "padding", "padding-left", "padding-right", "padding-top", "padding-bottom",
+    "border", "border-left", "border-right", "border-top", "border-bottom",
+    "width", "height", "max-width", "max-height",
+})
+
+css_sanitizer = CSSSanitizer(allowed_css_properties=ALLOWED_CSS_PROPERTIES)
 
 
 def sanitize_html(html: str) -> str:
@@ -20,6 +34,7 @@ def sanitize_html(html: str) -> str:
         html,
         tags=ALLOWED_TAGS,
         attributes=ALLOWED_ATTRIBUTES,
+        css_sanitizer=css_sanitizer,
         strip=True,
     )
     cleaned = bleach.linkify(cleaned)
